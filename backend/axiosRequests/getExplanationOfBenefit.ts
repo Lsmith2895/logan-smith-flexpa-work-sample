ac4368379cea60d8491f32f3aa08afd43cab7e07
Logan Smith <lsmith2895@yahoo.com>
2023-11-20 20:09:26 -0600

    more console logs

--- backend/axiosRequests/getExplanationOfBenefit.ts
+++ backend/axiosRequests/getExplanationOfBenefit.ts
@@ -1,7 +1,6 @@
 import axios, { AxiosResponse } from 'axios';
 
 export const getExplanationOfBenefit = async (accessToken: string): Promise<AxiosResponse> => {
-    console.log('********* ', accessToken)
     return axios.get(
         'https://api.flexpa.com/fhir/ExplanationOfBenefit',
         {
