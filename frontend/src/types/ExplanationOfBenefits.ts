2af823b582194dfe8ded045f46c9b935b7dd13be
Logan Smith <lsmith2895@yahoo.com>
2023-11-22 15:25:45 -0500

    create eob types (some bc this thibng is giagantic)

--- frontend/src/types/ExplanationOfBenefits.ts
+++ frontend/src/types/ExplanationOfBenefits.ts
@@ -1,17 +1,98 @@
 interface ExplanationOfBenefitBundle {
     resourceType: string;
-    id: string;
-    entry: ExplanationOfBenefitEntry[];
+    id?: string;
+    entry: Entry[];
+    type?: string;
 }
 
-interface ExplanationOfBenefitEntry {
-    url: string;
-    resource: ExplanationOfBenefitResource;
+interface Entry {
+    url?: string;
+    fullUrl: string;
+    resource: Resource;
 }
 
-interface ExplanationOfBenefitResource {
+interface Resource {
     resourceType: string;
-    // You can add more properties specific to ExplanationOfBenefitResource if needed
+    id: string;
+    meta: Meta;
+    contained: ContainedResource[];
 }
+
+type MetaTag = {
+    system: string;
+    code: string;
+  };
+  
+  type Meta = {
+    versionId: string;
+    lastUpdated: string;
+    source: string;
+    profile: string[];
+    tag: MetaTag[];
+  };
+  
+  type Identifier = {
+    system: string;
+    value: string;
+    type?: {
+      coding: {
+        system: string;
+        code: string;
+        display: string;
+      }[];
+    };
+  };
+  
+  export type Address = {
+    line: string[];
+    city: string;
+    state: string;
+    postalCode: string;
+    country?: string;
+  };
+  
+  type Telecom = {
+    system: string;
+    value: string;
+  };
+  
+  type Contact = {
+    name?: {
+      family: string;
+      given?: string[];
+    };
+    telecom?: Telecom[];
+  };
+  
+  type Organization = {
+    resourceType: string;
+    id: string;
+    meta?: {
+      profile: string[];
+    };
+    identifier?: Identifier[];
+    name?: string;
+    address?: Address[];
+    contact?: Contact[];
+  };
+  
+  type Practitioner = {
+    resourceType: string;
+    id: string;
+    meta?: {
+      profile: string[];
+    };
+    identifier?: Identifier[];
+    name?: {
+      family?: string;
+      given?: string[];
+    }[];
+    telecom?: Telecom[];
+    address?: Address[];
+  };
+  
+  type ContainedResource = Organization | Practitioner;
+  
+  
 
 export type { ExplanationOfBenefitBundle }
