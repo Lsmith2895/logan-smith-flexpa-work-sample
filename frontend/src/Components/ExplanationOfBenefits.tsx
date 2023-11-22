185cc39b31173ed27e8f2ae429973e21c0ab941d
Logan Smith <lsmith2895@yahoo.com>
2023-11-22 15:25:28 -0500

    update EOB component

--- frontend/src/Components/ExplanationOfBenefits.tsx
+++ frontend/src/Components/ExplanationOfBenefits.tsx
@@ -1,19 +1,51 @@
-import { ExplanationOfBenefitBundle } from "../types/ExplanationOfBenefits"
+/* eslint-disable @typescript-eslint/no-explicit-any */
+// import { ExplanationOfBenefitBundle } from "../types/ExplanationOfBenefits"
 
-interface explanationOfBenefitsProps {
-    mockData: ExplanationOfBenefitBundle
-}
+import { ReactElement } from "react"
+import { Address } from "../types/ExplanationOfBenefits"
 
-function ExplanationOfBenefits({mockData}: explanationOfBenefitsProps) {
+// interface explanationOfBenefitsProps {
+//     mockData: ExplanationOfBenefitBundle
+// }
 
-    return (
-        <div className="min-w">
-            <h1> Explanation of Benefit Data</h1>
-            <div>{`ID: ${mockData.id}`}</div>
-            <div>{`resourceType: ${mockData.entry[0].resource.resourceType}`}</div>
-            <button href={mockData.entry[0].url} className="bg-zinc-500 p-2 rounded-lg text-white"> link to my explanation of benefits</button>
-        </div>
-    )
+function ExplanationOfBenefits(mockData: any): ReactElement {
+    // console.log(mockData.mockData.entry[0].resource.contained[0])
+    const firstEntry = mockData.mockData.entry[0].resource
+    return firstEntry.contained.map((contained: any) => {
+        let contact = ''
+        const resourceAddress: Address = contained.address[0]
+
+        if (contained.contact && contained.contact.length > 0) {
+            contact = contained.contact[0]
+        }
+        if (contained.name && !contained.name.family) {
+            return (
+                <div className="m-5 bg-neutral-200 h-auto w-60 p-3 rounded-lg shadow-gray-400 shadow-xl border-gray-400" key={Math.random()}>
+                    {typeof contained.name === "string" && contained.resourceType && (
+                        <div className="mb-3">
+                            <div className="mb-2 font-bold">
+                                {contained.resourceType}:
+                            </div>
+                            <div>
+                                {contained.name}
+                            </div>
+                        </div>)}
+                    <div className="mb-3">
+                        <div className="mb-2 font-bold">Address:</div>
+                        <div> {resourceAddress.line.join(' ')} {resourceAddress.city}, {resourceAddress.state} {resourceAddress.postalCode}, {resourceAddress.country}</div>
+                    </div>
+                    {contact &&
+                        <div className="mb-3">
+                            <div className="mb-2 font-bold">Phone Number:</div>
+                            <div className="">{contained.contact[0].telecom[0].value}</div>
+                        </div>
+                    }
+                </div>
+            )
+        }
+    })
+    return (<></>)
+
 }
 
 export { ExplanationOfBenefits }
